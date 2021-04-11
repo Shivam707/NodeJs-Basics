22195a8a3ffea985ef7ed444faa6b246cb35b834
Shivam707 <42870315+Shivam707@users.noreply.github.com>
2021-04-11 13:41:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+const express = require('express');
+
+const app = express();
+
+const fetch = require("node-fetch");
+
+
+
+app.get('/api/git',(req, res)=>{
+    async function gitRepoApi(){
+        const response = await fetch('https://api.github.com/users');
+        const users = await response.json();
+        return users;
+    }
+    const a = gitRepoApi();
+    a.then(data =>{
+        if(err){
+            return err;
+        }
+       res.send(data);
+    })
+})
+
+app.listen(9090,() => {
+    console.log("Listening to 9090");
+})
