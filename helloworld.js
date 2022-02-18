25698882b722b60af6af2a13986e455cebf881c2
shivam707 <shivam967591@gmail.com>
2022-02-18 22:45:27 +0530

    just commited

---
+++ helloworld.js
@@ -0,0 +1,53 @@
+// var a1 = [{"id":1,"ph":1},{"id":2,"ph":2},{"id":3,"ph":3}];
+// var a2 = [{"id":1,"ph":1},{"id":2,"ph":2},{"id":4,"ph":4}];
+
+// for(var i = 0; i < a1.length; i++){
+//     for(var j = 0; j < a2.length; j++){
+//         if()
+//     }
+// }
+
+
+var arr = [1,2,3,5,4];
+
+arr.sort(); - O(n lon n)
+
+console.log(arr[3]); - O(1)
+
+find largest element and delete it .O
+
+a,b = b,a
+
+{1,100} -> {2,200} ->{3,300}
+
+Class list(self):
+    node = None,
+    head = node,
+    def createNode(self, index):
+        node.data = index
+        node.next = None
+        return node
+    def printList(node):
+        while(node):
+            print(node.data)   
+            node = node.next
+object = list()
+
+object.createNode(5)
+object.createNode(5)
+object.createNode(5)
+object.createNode(5)
+object.createNode(5)
+
+object.printList(head)
+
+    
+function fun(req,res,next){
+    var = req.body
+
+    return res.send();
+
+    next();
+}
+
+app.post(,,)
